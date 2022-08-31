7052cccf6787e8318046ee53a1bc0bc1b62fe871
ascottvhz <ascott@istar.ca>
2022-08-31 15:44:53 -0400

    Set the AutoSize property for the border settings form to true.

--- src/Options/BorderEditorDialog.Designer.cs
+++ src/Options/BorderEditorDialog.Designer.cs
@@ -200,6 +200,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(216, 291);
             this.ControlBox = false;
             this.Controls.Add(this.numBottom);
